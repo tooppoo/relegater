64cca479e5b2c4b7383602de87e6ba38c6a14338
tooppoo <Ryouta@kuribayashiryouta-no-MacBook-Pro.local>
2017-07-20 23:26:40 +0900

    add index file for typescript and update package.son
    - add keyword
    - rename repository
    - add index.ts to import with typescript

---
+++ index.ts
@@ -0,0 +1,6 @@
+import * as Relegater from './src/delegater';
+
+export type Delegater<T> = Relegater.Delegater<T>;
+export type $Delegater = Relegater.$Delegater;
+export const delegate = Relegater.delegate;
+export const $delegate = Relegater.$delegate;
